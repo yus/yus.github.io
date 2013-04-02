6216031abb2654a76b5f2c3e3bcce8feeb692293
yus <swiftcase@gmail.com>
2013-04-02 14:41:17 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,4 +1,3 @@
-console.log('This is main JS file.');
 /*
 *  Name: Yusdesign Kuler Feed
 *  License: CC-NC-ND 3.0 Unported
@@ -7,39 +6,37 @@ google.load('feeds', '1');
 google.load('webfont', '1');
 
 function initialize() {
+  WebFontConfig = {
+    google: { families: [ 'Lora::latin' ] }
+  };
 //title: 'Quickmaker Themes', title : 'Quickmaker Color Themes'
-var feed = new google.feeds.Feed('http://kuler-api.adobe.com/rss/search.cfm?searchQuery=userID:102986&itemsPerPage=50&key=5F8FD294DC6015C63AEF97E329246996');
-feed.setNumEntries(50);
-feed.load(function(result) {
-  if (!result.error) {
-    var container = document.getElementById('kulerfeed');
-    for (var i = 0; i < result.feed.entries.length; i++) {
-      var keystr = '&key=5F8FD294DC6015C63AEF97E329246996';
-      var entry = result.feed.entries[i];
-      var entryID = entry.link.slice(entry.link.lastIndexOf('/')+1);
-      var snipp = 'http://kuler-api.adobe.com/rss/png/generateThemePng.cfm?themeid='+entryID+keystr;
-      var div = document.createElement('div');
-      var span = document.createElement('span');
-      var img = document.createElement('img');
-      var a = document.createElement('a');
-      a.setAttribute('href', entry.link);
-      img.setAttribute('src', snipp);
-      a.setAttribute('class', 'flink');
-      div.setAttribute('class', 'fentry');
-      img.setAttribute('class', 'penta');
-      span.setAttribute('class', 'thitle');
-      span.appendChild(document.createTextNode(entry.title.slice(entry.title.lastIndexOf(':')+2)));
-      a.appendChild(img);
-      a.appendChild(span);
-      div.appendChild(a);
-      container.appendChild(div);
+  var feed = new google.feeds.Feed('http://kuler-api.adobe.com/rss/search.cfm?searchQuery=userID:102986&itemsPerPage=50&key=5F8FD294DC6015C63AEF97E329246996');
+  feed.setNumEntries(50);
+  feed.load(function(result) {
+    if (!result.error) {
+      var container = document.getElementById('kulerfeed');
+      for (var i = 0; i < result.feed.entries.length; i++) {
+        var keystr = '&key=5F8FD294DC6015C63AEF97E329246996';
+        var entry = result.feed.entries[i];
+        var entryID = entry.link.slice(entry.link.lastIndexOf('/')+1);
+        var snipp = 'http://kuler-api.adobe.com/rss/png/generateThemePng.cfm?themeid='+entryID+keystr;
+        var div = document.createElement('div');
+        var span = document.createElement('span');
+        var img = document.createElement('img');
+        var a = document.createElement('a');
+        a.setAttribute('href', entry.link);
+        img.setAttribute('src', snipp);
+        a.setAttribute('class', 'flink');
+        div.setAttribute('class', 'fentry');
+        img.setAttribute('class', 'penta');
+        span.setAttribute('class', 'thitle');
+        span.appendChild(document.createTextNode(entry.title.slice(entry.title.lastIndexOf(':')+2)));
+        a.appendChild(img);
+        a.appendChild(span);
+        div.appendChild(a);
+        container.appendChild(div);
+      }
     }
-  }
-});
-WebFont.load({
-  google: {
-    families: [ 'Inder::latin' ]
-  }
-});
+  });
 }
 google.setOnLoadCallback(initialize);
