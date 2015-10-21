54372cac7a8c9378f6147319edebf28a378a2352
yus <swiftcase@gmail.com>
2015-10-21 21:51:47 +0800

    Update yus.js

--- javascripts/yus.js
+++ javascripts/yus.js
@@ -1,2 +1,15 @@
 var $jq = jQuery.noConflict();
 
+/**
+   .b-widget-donate {
+        background: #242424 !important;
+    }
+    .b-widget-donate .b-input-text__input {
+        background: RGBA(72, 72, 72, 0.72) !important;
+    }
+ * ***/
+ 
+ $jq('.b-widget-donate').css({
+   'background-color':'#242424 !important',
+   'background-image':'none !important'
+ });
