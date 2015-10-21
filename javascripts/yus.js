e604a655e2b1bbf2e2c0f853da9ec03a136b3528
yus <swiftcase@gmail.com>
2015-10-21 22:59:22 +0800

    Update yus.js

--- javascripts/yus.js
+++ javascripts/yus.js
@@ -1,17 +1,11 @@
 /**mod**/
 
 $(document).ready(function(){
-    var widgetDonateStyle = {
-      background: "none !important",
-      backgroundSize: "0px 0%",
-      borderRadius: "0px",
-      boxShadow: "none"
-    };
+    var widgetDonateStyle = { background: "none !important" };
     
-    $("div.yndx > iframe").load(function() {
-        var frame = $("div.yndx > iframe").contents();
-        /* other code */
-        frame.find("div.b-widget-donate").css( widgetDonateStyle );
+    $("div#yndx > iframe").load(function() {
+        var frm = $("div#yndx > iframe").contents();
+        frm.find("div.b-widget-donate").css( widgetDonateStyle );
     });
     
 }); 
