014eb7f0d875e184678172f337098a0b9f6a62f3
yus <swiftcase@gmail.com>
2016-03-29 21:58:55 +0800

    Update beta.js

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -11,27 +11,22 @@ $j.ajax({
   url:qu,
   dataType: 'xml'
 }).done( function( response ) {
-  if (!response.error) {
+  if ( !response.error ) {
     var cntnr = $j( 'kulerfeed' );
     cntnr.empty();
     var items = $j( response ).find( 'item' );
     
     
     $j.each( items, function( index, value ) {
-      var entry = $j(value), qeystr = '&key=5F8FD294DC6015C63AEF97E329246996'; 
-      var qrf = entry.has('link').text(), qttl = entry.has('title').text(), qcapt = $j(value).has('description');
+      var entry = $j( value ), qeystr = '&key=5F8FD294DC6015C63AEF97E329246996'; 
+      var qrf = entry.has( 'link' ).text(), qttl = entry.has( 'title' ).text(), qcapt = $j( value ).has( 'description' );
       var lslcr = qrf.lastIndexOf('/')+1, tslcr = qttl.lastIndexOf(':')+2;
-      var entryID = qrf.slice(lslcr), entryTitle = qttl.slice(tslcr);
+      var entryID = qrf.slice( lslcr ), entryTitle = qttl.slice( tslcr );
       var snipp = 'https://kuler-api.adobe.com/rss/png/generateThemePng.cfm?themeid='+entryID+qeystr;
       
-      var qlink = cntnr.add('div').addClass('qi'+index)
-                        .add('a').addClass('ql')
-                        .attr('href', );
-      
-      qlink.add('img').addClass('q')
-            .attr('src', snipp)
-            .add('span').addClass('t')
-            .html(qttl);
+      var qlink = cntnr.add( 'div' ).addClass( 'qi'+ index ).add( 'a' ).addClass( 'ql' ).attr( 'href', qrf );
+      qlink.add( 'img' ).addClass( 'q' ).attr( 'src', snipp );
+      qlink.add( 'span' ).addClass('t').html( qttl );
       
       /*
       <div class="qi">
