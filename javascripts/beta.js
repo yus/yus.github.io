b249ddc3f06d1cfe06d7c144ab8d56f52e9a9c40
yus <swiftcase@gmail.com>
2016-03-30 03:28:59 +0800

    beta 29

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -5,7 +5,7 @@
 $j = jQuery.noConflict();
 var qc = '?searchQuery=userID:102986', qn = '&itemsPerPage=50', qk = '&key=5F8FD294DC6015C63AEF97E329246996';
 var qu = 'https://kuler-api.adobe.com/rss/search.cfm' + qc + qn + qk;
-var entry, entryTitle, themeLink, themeImageLink, entryID;
+var entry, entryTitle, themeLink, themeImageLink, entryID, quler;
 $j.ajax({ 
   url:qu,
   dataType: 'xml'
@@ -13,19 +13,14 @@ $j.ajax({
   if ( !response.error ) {
     var items = $j( response ).find( 'item' );
     $j.each( items, function( i, u ) {
+      quler = $j('<div id="qi'+i+'"></div>');
+      quler.appendTo('.gesso');
       entry = items[i];
       entryTitle = $j( $j(entry).find('title')[1] ).text();
       themeLink = $j( $j(entry).find('link')[0] ).text();
       themeImageLink = $j( $j(entry).find('link')[1] ).text();
       entryID = themeLink.slice( themeLink.lastIndexOf('/')+1 );
-      $j('.gesso').html( '<div id="qi'+i+'"></div>' );
-    }).done( function( i, entry, entryTitle, themeLink, themeImageLink, entryID ) {
-      return entry, entryTitle, themeLink, themeImageLink, entryID;
-      $j('div[id=^qi]'[i]).html( '<a class="ql'+i+'"></a>' );
-      $j('div[id=^qi]'[i]).find('a[class=^ql]').attr( 'href', themeLink )
-                          .html( '<img class="q"/><span class="t"></span>' );
-      $j('div[id=^qi]'[i]).find('img.q').attr( 'src', themeImageLink );
-      $j('div[id=^qi]'[i]).find('span.t').html( entryTitle );
+      quler.html('<a href="'+themeLink+'"><img src="'+themeImageLink+'"/><span>'+entryTitle+'</span></a>');
       console.log( i + ' > ' + typeof entry + ' >> ' + themeLink + ' >> ' + entryTitle + ' <' );
       console.log( i + ' > ' + typeof entry + ' >> ' + themeImageLink + ' >> ' + entryID + ' <' );
     });
