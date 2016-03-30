05f74786dc145489fab9daa18555cae4a4d17990
yus <swiftcase@gmail.com>
2016-03-30 23:35:43 +0800

    Update beta.js

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -2,13 +2,57 @@
 *  Name: Yusdesign Kuler Feed
 *  License: CC-NC-ND 3.0 Unported
 */
+var cntnr, rendrr, mg, pg, wW, wH, singulars, halves, anchors, q;
 
-var cntnr, rendrr, mg, pg, wW, wH, singulars, halves, ilinks;
-var entry, entryTitle, themeLink, themeImageLink, entryID, quler;
+function preload() {
+  if( !anchors ) ? anchors.append( q );
+  mg = 5, pg = 20;
+  wW = windowWidth - 2*pg - 2*mg;
+  wH = windowHeight - 2*pg - 2*mg;
+  for(var j = 0; j < anchors.length; j++){
+    singulars.append( loadImage( anchors[j] ) );
+  }
+}
+
+function setup() {
+  createCanvas( wW, wH );
+  rendrr = createGraphics( wW, wH );
+  cntnr = createDiv( "thinking up the tints" ).id( "bg" ).class( "bg" )
+          .style( "background-color", "rgba(25, 25, 25, .29)" )
+          .style( "color", "rgba(255, 129, 29, .29)" )
+          .style( "overflow-x", "hidden" )
+          .style( "overflow-y", "auto" )
+          .style( "font-family", "'Fira Sans', sans-serif" )
+          .style( "font-size", "11px" );
+  for( var r = 0; r < singulars.length; r++ ){
+    push();
+    singulars[r].loadPixels();
+    halves.append( 4 * width * height/2 );
+    for( var f = 0; f < halves.length; f++ ){
+      singulars[r].pixels[f+halves[f]] = singulars[r].pixels[f];
+    }
+    singulars[r].updatePixels();
+  }
+}
+
+function draw() {
+  cntnr.size( wW, wH )
+        .style( "padding", pg+"px" )
+        .style( "margin", mg+"px" );
+  //rendrr.beginDraw();
+  for( var s = 0; s < singulars.length; s++ ){
+    rendrr.image( singulars[s], 0*s, 0*s );
+  }
+}
+
+function windowResized() {
+  resizeCanvas( wW, wH );
+}
 
 $.noConflict();
 (function( $ ) {
   $(function() {
+    var entry, entryTitle, themeLink, themeImageLink, entryID, quler;
     // More code using $ as alias to jQuery
     var qc = '?searchQuery=userID:102986', qn = '&itemsPerPage=50', qk = '&key=5F8FD294DC6015C63AEF97E329246996';
     var qu = 'https://kuler-api.adobe.com/rss/search.cfm' + qc + qn + qk;
@@ -19,16 +63,16 @@ $.noConflict();
       if ( !response.error ) {
         var items = $( response ).find( 'item' );
         $.each( items, function( i, u ) {
-          quler = $('<div id="qi'+i+'"></div>');
-          quler.css('font-family', '"Fira Sans", sans-serif').appendTo('.gesso');
+          quler = $( '<div id="qi'+i+'"></div>' );
+          quler.css( 'font-family', '"Fira Sans", sans-serif' ).appendTo( '.gesso' );
           entry = items[i];
-          themeImageLink = $( $(entry).find('link')[1] ).text();
-          $( ilinks ).push(themeImageLink);
-          entryTitle = $( $(entry).find('title')[1] ).text();
-          themeLink = $( $(entry).find('link')[0] ).text();
+          themeImageLink = $( $( entry ).find( 'link' )[1] ).text();
+          q = themeImageLink;
+          entryTitle = $( $( entry ).find( 'title' )[1] ).text();
+          themeLink = $( $( entry ).find( 'link' )[0] ).text();
           entryID = themeLink.slice( themeLink.lastIndexOf('/')+1 );
-          quler.html('<a href="'+themeLink+'"><img src="'+themeImageLink+'"/><span>'+entryTitle+'</span></a>');
-          return ilinks;
+          quler.html( '<a href="'+themeLink+'"><img src="'+themeImageLink+'"/><span>'+entryTitle+'</span></a>' );
+          return q;
           //console.log( i + ' > ' + typeof entry + ' >> ' + themeLink + ' >> ' + entryTitle + ' <' );
           //console.log( i + ' > ' + typeof entry + ' >> ' + themeImageLink + ' >> ' + entryID + ' <' );
         });
@@ -37,46 +81,3 @@ $.noConflict();
     console.log( ilinks );
   });
 })(jQuery);
-
-function preload() {
-  mg = 5, pg = 20;
-  wW = windowWidth - 2*pg - 2*mg;
-  wH = windowHeight - 2*pg - 2*mg;
-  for(var j = 0; j < ilinks.length; j++){
-    singulars.push(loadImage(ilinks[j]));
-  }
-}
-
-function setup() {
-  createCanvas( wW, wH );
-  rendrr = createGraphics( wW, wH );
-  cntnr = createDiv( "thinking up the tints" ).id("bg").class("bg")
-          .style("background-color", "rgba(25, 25, 25, .29)")
-          .style("color", "rgba(255, 129, 29, .29)")
-          .style("overflow-x", "hidden")
-          .style("overflow-y", "auto")
-          .style("font-family", "'Fira Sans', sans-serif")
-          .style("font-size", "11px");
-  for(var r = 0; r < singulars.length; r++){
-    singulars[r].loadPixels();
-    halves.push(4 * width * height/2);
-    for(var f = 0; f < $halves.length; f++){
-      singulars[r].pixels[f+halves[f]] = singulars[r].pixels[f];
-    }
-    singulars[r].updatePixels();
-  }
-}
-
-function draw() {
-  cntnr.size( wW, wH )
-        .style("padding", pg+"px")
-        .style("margin", mg+"px");
-  //rendrr.beginDraw();
-  for(var s = 0; s < singulars.length; s++){
-    rendrr.image( singulars[s], 0*s, 0*s );
-  }
-}
-
-function windowResized() {
-  resizeCanvas( wW, wH );
-}
