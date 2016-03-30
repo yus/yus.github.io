bcd5a63738beb7e2ec852cb8261bf8273761d18f
yus <swiftcase@gmail.com>
2016-03-30 18:17:53 +0800

    Update beta.js

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -2,19 +2,19 @@
 *  Name: Yusdesign Kuler Feed
 *  License: CC-NC-ND 3.0 Unported
 */
+var $q = $.Deffered();
+var $j = {}, $d, $q; 
+$d = $j.query(document);
+$j.query = jQuery.noConflict(true);
 
-var $j = {}, $jq, $d, $q; 
-$jq = $j.query;
-$jq = jQuery.noConflict(true);
-$d = $jq(document);
-$q = $jq.Deffered();
+
 
 var cntnr, rendrr, mg, pg, wW, wH, singulars, halves, ilinks;
 var entry, entryTitle, themeLink, themeImageLink, entryID, quler;
 
 function preload( ilinks ) {
-  for(var j = 0; j < $jq( ilinks ).length; j++){
-    $jq(singulars).push(loadImage($jq(ilinks)[j]));
+  for(var j = 0; j < $j.query( ilinks ).length; j++){
+    $j.query(singulars).push(loadImage($j.query(ilinks)[j]));
   }
 }
 
@@ -32,13 +32,13 @@ function setup( singulars ) {
           .style("font-family", "'Fira', sans-serif")
           .style("font-size", "11px");
 
-  for(var r = 0; r < $jq(singulars).length; r++){
-    $jq(singulars)[r].loadPixels();
-    $jq(halves).push(4 * width * height/2);
+  for(var r = 0; r < $j.query(singulars).length; r++){
+    $j.query(singulars)[r].loadPixels();
+    $j.query(halves).push(4 * width * height/2);
     for(var f = 0; f < $j(halves).length; f++){
-      $jq(singulars)[r].pixels[f+$j(halves)[f]] = $jq(singulars)[r].pixels[f];
+      $j.query(singulars)[r].pixels[f+$j.query(halves)[f]] = $j.query(singulars)[r].pixels[f];
     }
-    $jq(singulars)[r].updatePixels();
+    $j.query(singulars)[r].updatePixels();
   }
 }
 
@@ -47,8 +47,8 @@ function draw( singulars, wW, wH, mg, pg ) {
         .style("padding", pg+"px")
         .style("margin", mg+"px");
   //rendrr.beginDraw();
-  for(var s = 0; s < $jq(singulars).length; s++){
-    rendrr.image($jq(singulars)[s], 0*s, 0*s);
+  for(var s = 0; s < $j.query(singulars).length; s++){
+    rendrr.image($j.query(singulars)[s], 0*s, 0*s);
   }
 }
 
@@ -58,23 +58,23 @@ function windowResized(wW, wH) {
 
 //$q.done( preload, setup, draw, windowResized, $j );
 $q.done( preload, setup, draw, windowResized );
-$d.ready( function( $jq ) {
+$d.ready( function( $ ) {
 var qc = '?searchQuery=userID:102986', qn = '&itemsPerPage=50', qk = '&key=5F8FD294DC6015C63AEF97E329246996';
 var qu = 'https://kuler-api.adobe.com/rss/search.cfm' + qc + qn + qk;
-$jq.ajax({ 
+$j.query.ajax({ 
   url:qu,
   dataType: 'xml'
 }).done( function( response ) {
   if ( !response.error ) {
-    var items = $jq( response ).find( 'item' );
-    $jq.each( items, function( i, u ) {
-      quler = $jq('<div id="qi'+i+'"></div>');
+    var items = $j.query( response ).find( 'item' );
+    $j.query.each( items, function( i, u ) {
+      quler = $j.query('<div id="qi'+i+'"></div>');
       quler.appendTo('.gesso');
       entry = items[i];
-      themeImageLink = $jq( $jq(entry).find('link')[1] ).text();
-      $jq( ilinks ).push(themeImageLink);
-      entryTitle = $jq( $jq(entry).find('title')[1] ).text();
-      themeLink = $jq( $jq(entry).find('link')[0] ).text();
+      themeImageLink = $j.query( $j.query(entry).find('link')[1] ).text();
+      $j.query( ilinks ).push(themeImageLink);
+      entryTitle = $j.query( $j.query(entry).find('title')[1] ).text();
+      themeLink = $j.query( $j.query(entry).find('link')[0] ).text();
       entryID = themeLink.slice( themeLink.lastIndexOf('/')+1 );
       quler.html('<a href="'+themeLink+'"><img src="'+themeImageLink+'"/><span>'+entryTitle+'</span></a>');
       console.log( i + ' > ' + typeof entry + ' >> ' + themeLink + ' >> ' + entryTitle + ' <' );
