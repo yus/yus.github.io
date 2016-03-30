c90b125a38c5383729485189f2f5b74dd48b1323
yus <swiftcase@gmail.com>
2016-03-30 17:12:05 +0800

    Update beta.js

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -3,12 +3,12 @@
 *  License: CC-NC-ND 3.0 Unported
 */
 
-var cntnr, mg, pg, wW, wH, qu, title, caption, $qlr, $j;
+var cntnr, rendrr, mg, pg, wW, wH, singulars, halves, ilinks;
+var $j, entry, entryTitle, themeLink, themeImageLink, entryID, quler;
 
 $j = jQuery.noConflict();
 var qc = '?searchQuery=userID:102986', qn = '&itemsPerPage=50', qk = '&key=5F8FD294DC6015C63AEF97E329246996';
 var qu = 'https://kuler-api.adobe.com/rss/search.cfm' + qc + qn + qk;
-var entry, entryTitle, themeLink, themeImageLink, singleImages=[], halfImages=[], ilinks=[], entryID, quler;
 
 $j.ajax({ 
   url:qu,
@@ -36,15 +36,16 @@ console.log( ilinks )
 
 function preload( ilinks ) {
   for(var j = 0; j < $j( ilinks ).length; j++){
-    $j(singleImages).push(loadImage($j(ilinks)[j]));
+    $j(singulars).push(loadImage($j(ilinks)[j]));
   }
 }
 
-function setup( singleImages ) {
+function setup( singulars ) {
   mg = 5, pg = 20;
   wW = windowWidth - 2*pg - 2*mg;
   wH = windowHeight - 2*pg - 2*mg;
   createCanvas(wW, wH);
+  rendrr = createGraphics(wW, wH);
   cntnr = createDiv("thinking up the tints").id("bg").class("bg")
           .style("background-color", "rgba(25, 25, 25, .29)")
           .style("color", "rgba(255, 129, 29, .29)")
@@ -53,22 +54,23 @@ function setup( singleImages ) {
           .style("font-family", "'Fira', sans-serif")
           .style("font-size", "11px");
 
-  for(var r = 0; r < $j(singleImages).length; r++){
-    $j(singleImages)[r].loadPixels();
-    $j(halfImages).push(4 * width * height/2);
-    for(var f = 0; f < $j(halfImages).length; f++){
-      $j(singleImages)[r].pixels[f+$j(halfImages)[f]] = $j(singleImages)[r].pixels[f];
+  for(var r = 0; r < $j(singulars).length; r++){
+    $j(singulars)[r].loadPixels();
+    $j(halves).push(4 * width * height/2);
+    for(var f = 0; f < $j(halves).length; f++){
+      $j(singulars)[r].pixels[f+$j(halves)[f]] = $j(singulars)[r].pixels[f];
     }
-    $j(singleImages)[r].updatePixels();
+    $j(singulars)[r].updatePixels();
   }
 }
 
-function draw( singleImages, wW, wH ) {
+function draw( singulars, wW, wH, mg, pg ) {
   cntnr.size(wW, wH)
         .style("padding", pg+"px")
         .style("margin", mg+"px");
-  for(var s = 0; s < $j(singleImages).length; s++){
-    image($j(singleImages)[s], 0*s, 0*s);
+  rendrr.beginDraw();
+  for(var s = 0; s < $j(singulars).length; s++){
+    rendrr.image($j(singulars)[s], 0*s, 0*s);
   }
 }
 
