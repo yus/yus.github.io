0972bb86211f89eaeabd9e444ef62278bd848650
yus <swiftcase@gmail.com>
2016-03-30 17:42:21 +0800

    Update beta.js

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -4,7 +4,7 @@
 */
 var cntnr, rendrr, mg, pg, wW, wH, singulars, halves, ilinks;
 var $j = {}, $q = $.Deffered();
-$j = jQuery.noConflict(true);
+$j = jQuery.noConflict();
 var $d = $j(document);
 var entry, entryTitle, themeLink, themeImageLink, entryID, quler;
 var qc = '?searchQuery=userID:102986', qn = '&itemsPerPage=50', qk = '&key=5F8FD294DC6015C63AEF97E329246996';
@@ -54,31 +54,30 @@ function windowResized(wW, wH) {
   resizeCanvas(wW, wH);
 }
 
-$j.ajax({ 
-  url:qu,
-  dataType: 'xml'
-}).done( function( response ) {
-  if ( !response.error ) {
-    var items = $j( response ).find( 'item' );
-    $j.each( items, function( i, u ) {
-      quler = $j('<div id="qi'+i+'"></div>');
-      quler.appendTo('.gesso');
-      entry = items[i];
-      themeImageLink = $j( $j(entry).find('link')[1] ).text();
-      $j( ilinks ).push(themeImageLink);
-      entryTitle = $j( $j(entry).find('title')[1] ).text();
-      themeLink = $j( $j(entry).find('link')[0] ).text();
-      entryID = themeLink.slice( themeLink.lastIndexOf('/')+1 );
-      quler.html('<a href="'+themeLink+'"><img src="'+themeImageLink+'"/><span>'+entryTitle+'</span></a>');
-      console.log( i + ' > ' + typeof entry + ' >> ' + themeLink + ' >> ' + entryTitle + ' <' );
-      console.log( i + ' > ' + typeof entry + ' >> ' + themeImageLink + ' >> ' + entryID + ' <' );
-    });
-  }
-});
-
 $q.done( preload, setup, draw, windowResized, $j );
 //$q.done( $j, preload, setup, draw, windowResized );
-$d.ready(function( $j, ilinks ) {
+$d.ready( function($) {
+  $j.ajax({ 
+    url:qu,
+    dataType: 'xml'
+  }).done( function( response ) {
+    if ( !response.error ) {
+      var items = $j( response ).find( 'item' );
+      $j.each( items, function( i, u ) {
+        quler = $j('<div id="qi'+i+'"></div>');
+        quler.appendTo('.gesso');
+        entry = items[i];
+        themeImageLink = $j( $j(entry).find('link')[1] ).text();
+        $j( ilinks ).push(themeImageLink);
+        entryTitle = $j( $j(entry).find('title')[1] ).text();
+        themeLink = $j( $j(entry).find('link')[0] ).text();
+        entryID = themeLink.slice( themeLink.lastIndexOf('/')+1 );
+        quler.html('<a href="'+themeLink+'"><img src="'+themeImageLink+'"/><span>'+entryTitle+'</span></a>');
+        console.log( i + ' > ' + typeof entry + ' >> ' + themeLink + ' >> ' + entryTitle + ' <' );
+        console.log( i + ' > ' + typeof entry + ' >> ' + themeImageLink + ' >> ' + entryID + ' <' );
+      });
+    }
+  });
   $q.resolve( ilinks );
   console.log( ilinks );
 });
