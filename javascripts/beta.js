2c1a0f0e1eb95c1325c91df800f083169e3ab8f1
yus <swiftcase@gmail.com>
2016-04-16 14:43:22 +0800

    beta29

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -13,42 +13,14 @@ jQuery.noConflict();
     qk = '&key=5F8FD294DC6015C63AEF97E329246996';
     var qu = 'https://kuler-api.adobe.com/rss/search.cfm' + qc + qn + qk;
 
-    function jeesonify(result){
-      var xml = result;
-      var rgx = /(<\w+[^<]*?)\s+([\w-]+)="([^"]+)">/;
-      while (xml.match(rgx)) xml = xml.replace(rgx, '<$2>$3</$2>$1>');  
-      xml = xml.replace(/\s/g, ' ')
-        .replace(/< *\?[^>]*?\? *>/g, '')
-        .replace(/< *!--[^>]*?-- *>/g, '')
-        .replace(/< *(\/?) *(\w[\w-]+\b):(\w[\w-]+\b)/g, '<$1$2_$3')
-        .replace(/< *(\w[\w-]+\b)([^>]*?)\/ *>/g, '< $1$2>')
-        .replace(/(\w[\w-]+\b):(\w[\w-]+\b) *= *"([^>]*?)"/g, '$1_$2="$3"')
-        .replace(/< *(\w[\w-]+\b)((?: *\w[\w-]+ *= *" *[^"]*?")+ *)>( *[^< ]*?\b.*?)< *\/ *\1 *>/g, '< $1$2 value="$3">')
-        .replace(/< *(\w[\w-]+\b) *</g, '<$1>< ').replace(/> *>/g, '>')
-        .replace(/"/g, '\\"')
-        .replace(/< *(\w[\w-]+\b) *>([^<>]*?)< *\/ *\1 *>/g, '"$1":"$2",')
-        .replace(/< *(\w[\w-]+\b) *>([^<>]*?)< *\/ *\1 *>/g, '"$1":[{$2}],')
-        .replace(/< *(\w[\w-]+\b) *>(?=("\w[\w-]+\b)":\{.*?\},\2)(.*?)< *\/ *\1 *>/, '"$1":{}$3},')
-        .replace(/],\s*?".*?": *\[/g, ',')
-        .replace(/< \/(\w[\w-]+\b)\},\{\1>/g, '},{')
-        .replace(/< *(\w[\w-]+\b)[^>]*?>/g, '"$1":{')
-        .replace(/< *\/ *\w[\w-]+ *>/g, '},')
-        .replace(/\} *,(?= *(\}|\]))/g, '}')
-        .replace(/] *,(?= *(\}|\]))/g, ']')
-        .replace(/" *,(?= *(\}|\]))/g, '"')
-        .replace(/ *, *$/g, '');
-      var json = '{' + xml + '}';
-      return json;
-    }
-    
     $.ajax({
       type: 'GET',  
       url: qu,
-      dataType: 'text'
-    }).done(function( responseText ){
-      if ( !responseText.error ){
-        var jsn = jeesonify( responseText );
-        console.log( typeof jsn );
+      dataType: 'xml'
+    }).done(function( responseXML){
+      if ( !responseXML.error ){
+        var $book = $( responseXML ).find( 'item' );
+        console.log( $book );
         //$('.gesso').html( jsn );
       }
     });
