621d2ab68f378ec5fa6bce3da760004d6bd11682
yus <swiftcase@gmail.com>
2016-04-16 10:03:32 +0800

    beta29

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -12,46 +12,24 @@ jQuery.noConflict();
     qn = '&itemsPerPage=50',
     qk = '&key=5F8FD294DC6015C63AEF97E329246996';
     var qu = 'https://kuler-api.adobe.com/rss/search.cfm' + qc + qn + qk;
+    
     $.ajax({
-      data: null,
-      dataType: 'xml',
-      url: qu,
-      dataFilter: function('kuler'){ return resp; }
-    }).done(function (resp) {
-      if (!resp.error) {
-        var items = $(resp).find('themeItem').get(),
-        ns_items = $(resp).find('kuler\\:themeItem').get();
-        var $items = undefined ? items : ns_items;
-        console.log( $($items).text() ); // typeof 
-        //console.log( $(response, document.item) );
-        
-        $.each( $items, function(q, r){
-          var rr, ettl, tID, thS, ns_thS, swtch, ns_swtch;
-          rr = undefined ? rr = $items[q] : rr = $(r)[q];
-          console.log(rr);
-          
-          thS = $(rr).find('themeSwatches');
-          ns_thS = $(rr).find('kuler\\:themeSwatches');
-          swtch = $(thS).find('swatch');
-          ns_swtch = $(ns_thS).find('kuler\\:swatch');
-          
-          ettl = $(rr).find( 'themeTitle' ).text();
-          tID = $(rr).find( 'themeID' ).text();
-          ns_ettl = $(rr).find( 'kuler\\:themeTitle' ).text();
-          ns_tID = $(rr).find( 'kuler\\:themeID' ).text();
-          console.log( q + ' ››› ' + (ettl || ns_ettl) + ' ››› ' + (tID || ns_tID) );
+      type: "GET",  
+      url: qu,  
+      dataType: "jsonp",
+      dataFilter: function(data, type){
+        if(type == "xml") console.log("returned xml!");
+           // Firebug complains about this
+        var newdata = data.replace(/</g, '{');
+        newdata = newdata.replace(/>/g, '}');
+        return newdata;
+      },
+      jsonp: "callback",
+      jsonpCallback: "jsonpcallback"
+    });  
+    function jsonpcallback(returndata) {
+      console.log( "data returned: " + returndata );
+    };
 
-          console.log(typeof thS + ' ››› ' + typeof swtch);
-
-          swtch.each(function(h, sclr){
-            var $sclr, $quartz;
-            $sclr = $(this);
-            $quartz = $($sclr).find('swatchHexColor').text() || $($sclr).find('kuler\\:swatchHexColor').text();
-            console.log( $quartz );
-          });
-          console.log('--------------------------------------------------------------------------------');
-        });
-      } // if !error
-    }); // ajax done
   });
 }) (jQuery);
