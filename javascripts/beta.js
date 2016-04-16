c29c12302fa9cf15484cc89d59b607eb0b325b76
yus <swiftcase@gmail.com>
2016-04-16 19:27:09 +0800

    beta29

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -19,27 +19,10 @@ jQuery.noConflict();
       dataType: 'xml'
     }).done(function( result ){
       if ( !result.error ){
-        var $books = $( result ).find( 'item' );
-        $.each($books, function( i,j ){
-          var $book = $(this);
-          var $q = $($book).find('kuler\\:themeID' || 'themeID')[0];
-          var $l = $($book).find('kuler\\:themeTitle' || 'themeTitle')[0];
-          var $swatches = $($book).find('kuler\\:swatch' || 'swatch');
-          $.each($swatches, function( k,l ){
-            var $swtch = $(this);
-            var $r = $($swtch).find('kuler\\:swatchChannel1' || 'swatchChannel1')[0];
-            var $g = $($swtch).find('kuler\\:swatchChannel2' || 'swatchChannel2')[0];
-            var $b = $($swtch).find('kuler\\:swatchChannel3' || 'swatchChannel3')[0];
-            var $a = $($swtch).find('kuler\\:swatchChannel4' || 'swatchChannel4')[0];
-            console.log( ' R ››› ' + $($r).html() );
-            console.log( ' G ››› ' + $($g).html() );
-            console.log( ' B ››› ' + $($b).html() );
-            console.log( ' A ››› ' + $($a).html() );
-          });
-          console.log( $($q).html() + ' ››› ' + $($l).html() );
-          //$('.gesso').html( jsn );
-        });  
-      }
+         swatchSheave(result);      }
     });
   });
 }) (jQuery);
+
+function swatchSheave(c){c=$(c).find("item");$.each(c,function(c,g){var a=$(this),d=$(a).find("kuler\\:themeID"),e=$(a).find("kuler\\:themeTitle"),a=$(a).find("kuler\\:swatch");$.each(a,function(a,c){var b=$(this),d=$(b).find("kuler\\:swatchChannel1"),e=$(b).find("kuler\\:swatchChannel2"),f=$(b).find("kuler\\:swatchChannel3"),b=$(b).find("kuler\\:swatchChannel4");console.log(" R \u203a\u203a\u203a "+$(d).html());console.log(" G \u203a\u203a\u203a "+$(e).html());console.log(" B \u203a\u203a\u203a "+$(f).html());
+console.log(" A \u203a\u203a\u203a "+$(b).html())});console.log($(d).html()+" \u203a\u203a\u203a "+$(e).html())})};
