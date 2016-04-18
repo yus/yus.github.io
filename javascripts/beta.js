b63d61fc747c05b135cead6d25a41a2654a04530
yus <swiftcase@gmail.com>
2016-04-18 22:31:50 +0800

    beta29

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -3,15 +3,16 @@
  *  License: CC-NC-ND 3.0 Unported
  */
 var utistor, cnv, img, cntnr, gesso;
-var cW, cH, rc, bg, d, sclr;
+var cW, cH, rc, bg, d, sclr, tinge;
 var distances = [], maxDistance, spacer;
 
 //* Yusdesign jQuery Kuler Feed *// 
 jQuery.noConflict();
 (function(a){a(function(){a("body").addClass("yusdesign");var g,d,h,k,l,e,m,c,f;a.ajax({type:"GET",url:"https://kuler-api.adobe.com/rss/search.cfm?searchQuery=userID:102986&itemsPerPage=50&key=5F8FD294DC6015C63AEF97E329246996",dataType:"xml"}).done(function(b){b.error||(b=a(b).find("item"),a.each(b,function(b,n){g=a(this);d=g[0];h=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","themeID")[0].valueOf().innerHTML.toString();k=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","themeTitle")[0].valueOf().innerHTML.toString();
 m="//color.adobe.com/themeID/"+h;e=a('<div id="quartz'+b+'"></div>').addClass("tinge");c=a('<div id="title'+b+'"></div>').addClass("tetra");f=a("<a>").attr("href",m).addClass("tange");f.append(a("<span>").text(k).addClass("titre"));c.append(f);l=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","swatch");a.each(l,function(b,d){var c=a(this)[0].getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","swatchHexColor")[0].valueOf().innerHTML.toString();e.append(a("<div>").css("background-color",
-"#"+c).addClass("scalar"))});c.append(e);a("div#kulerfeed").append(c)}))})})})(jQuery);
+"#"+c).addClass("scalar"))});c.append(e);a("div#kulerfeed").append(c);tinge = $.each('div[id^="quartz"]').next('.scalar').css('background-color')}))})})})(jQuery);
 
+var $j = jQuery.noConflict();
 function preload() {
   gesso = select('#gesso');
   createDiv('').id('cntnr').parent(gesso);
@@ -47,13 +48,12 @@ function draw() {
     img.pixels[i + 2] = blue(rc);
     img.pixels[i + 3] = alpha(rc);
   }
-  
-  for (q = 0; q < img.width; q++) {
-    var quartz = select('#quartz'+q);
-    for (r = 0; r < img.height; r++) {
-      var tinge = quartz.getElementsByClassName('scalar');
-      var tng = color(tinge.style('background-color'));
-      img.set(q, r, tng);
+  for (w = 0; w < img.width; w++) {
+    for (h = 0; h < img.height; h++) {
+      for (t = 0; t <= tinge; t++) {
+         var tng = tinge[t];
+         img.set(w, h, color(tng));
+      }
     }
   }
   img.updatePixels();
