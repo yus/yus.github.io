4ba74f9f48fc949f15c092a8d56fdc13fec103fe
yus <swiftcase@gmail.com>
2016-04-18 10:32:44 +0800

    beta29

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -27,7 +27,7 @@ jQuery.noConflict();
           $tID,
           $tTtl,
           $swatches;
-          $ns_tID = this.getElementsByTagNameNS(ns, 'themeID')[1].value;
+          $ns_tID = book.getElementsByTagNameNS(ns, 'themeID')[0].toString();
           !$tID ? $tID = $(jee).find('themeID') : $tID = $(jee).find('kuler\\:themeID');
           !$tTtl ? $tTtl = $(jee).find('themeTitle') : $tTtl = $(jee).find('kuler\\:themeTitle');
           console.log($ns_tID);
