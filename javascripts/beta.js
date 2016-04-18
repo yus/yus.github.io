d28f2864b1959e1d3ebf9f3c4e2dd0ac6812a92b
yus <swiftcase@gmail.com>
2016-04-18 21:06:49 +0800

    beta29

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -7,9 +7,10 @@ var cW, cH, rc, bg, d, scalar;
 var distances = [], maxDistance, spacer;
 
 //* Yusdesign jQuery Kuler Feed *// 
-jQuery.noConflict();(function(a){a(function(){a("body").addClass("ysdsgn");a.ajax({type:"GET",url:"https://kuler-api.adobe.com/rss/search.cfm?searchQuery=userID:102986&itemsPerPage=50&key=5F8FD294DC6015C63AEF97E329246996",dataType:"xml"}).done(function(b){b.error||(b=a(b).find("item"),a.each(b,function(b,h){var e=a(this)[0],c,g,f,d;c=e.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","themeID")[0].valueOf().innerHTML.toString();g=e.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","themeTitle")[0].valueOf().innerHTML.toString();
-d=d="//color.adobe.com/themeID/"+c;themeImageLink="//color.adobe.com/kuler/themeImages/theme_"+c+".png";f=a('<div id="quartz'+b+'"></div>').addClass("tinge");c=a('<div id="title'+b+'"></div>').addClass("fentry");d=a("<a>").attr("href",d).addClass("flink");d.append(a("<span>").text(g).addClass("thitle"));c.append(d);e=e.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","swatch");a.each(e,function(b,c){var d=a(this)[0].getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","swatchHexColor")[0].valueOf().innerHTML.toString();
-f.append(a("<div>").css({"background-color":"#"+d}).addClass("scalar"))});c.append(f);a("div#kulerfeed").append(c)}))})})})(jQuery);
+jQuery.noConflict();
+(function(a){a(function(){a("body").addClass("yusdesign");var g,d,h,k,l,e,m,c,f;a.ajax({type:"GET",url:"https://kuler-api.adobe.com/rss/search.cfm?searchQuery=userID:102986&itemsPerPage=50&key=5F8FD294DC6015C63AEF97E329246996",dataType:"xml"}).done(function(b){b.error||(b=a(b).find("item"),a.each(b,function(b,n){g=a(this);d=g[0];h=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","themeID")[0].valueOf().innerHTML.toString();k=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","themeTitle")[0].valueOf().innerHTML.toString();
+m="//color.adobe.com/themeID/"+h;e=a('<div id="quartz'+b+'"></div>').addClass("tinge");c=a('<div id="title'+b+'"></div>').addClass("tetra");f=a("<a>").attr("href",m).addClass("tange");f.append(a("<span>").text(k).addClass("titre"));c.append(f);l=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","swatch");a.each(l,function(b,d){var c=a(this)[0].getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","swatchHexColor")[0].valueOf().innerHTML.toString();e.append(a("<div>").css("background-color",
+"#"+c).addClass("scalar"))});c.append(e);a("div#kulerfeed").append(c)}))})})})(jQuery);
 
 function preload() {
   gesso = select('#gesso');
@@ -78,6 +79,5 @@ function utistor() {
 function cntnrSize() {
   cH = cntnr.height;
   cW = cntnr.width;
-  return cH,
-  cW;
+  return cH, cW;
 }
