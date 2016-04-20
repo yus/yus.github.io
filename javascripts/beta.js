5872b2f92b3f81b43517cb07a0bc3ec97bf2cebb
yus <swiftcase@gmail.com>
2016-04-20 21:05:26 +0800

    beta29

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -4,13 +4,13 @@
  */
 var utistor, cnv, img, cntnr, gesso;
 var cW, cH, rc, bg, d, sclr, tinge, tincture;
-var tinges = [], distances = [], maxDistance, spacer;
+var tumultu = [], distances = [], maxDistance, spacer;
 
 // Yusdesign jQuery Kuler Feed
 jQuery.noConflict();
-(function(a){a(function(){a("body").addClass("yusdesign");var g,d,h,k,l,e,m,c,f;a.ajax({type:"GET",url:"https://kuler-api.adobe.com/rss/search.cfm?searchQuery=userID:102986&itemsPerPage=50&key=5F8FD294DC6015C63AEF97E329246996",dataType:"xml"}).done(function(b){b.error||(b=a(b).find("item"),a.each(b,function(b,n){g=a(this);d=g[0];h=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","themeID")[0].valueOf().innerHTML.toString();k=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","themeTitle")[0].valueOf().innerHTML.toString();
-m="http://color.adobe.com/themeID/"+h;e=a('<div id="quartz'+b+'"></div>').addClass("tinge");c=a('<div id="title'+b+'"></div>').addClass("tetra");f=a("<a>").attr("href",m).addClass("tange");f.append(a("<span>").text(k).addClass("titre"));c.append(f);l=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","swatch");a.each(l,function(b,d){var c=a(this)[0].getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","swatchHexColor")[0].valueOf().innerHTML.toString();e.append(a("<div>").css("background-color",
-"#"+c).addClass("scalar"))});c.append(e);a("div#kulerfeed").append(c)}))})})})(jQuery);
+(function(a){a(function(){a("body").addClass("yusdesign");var g,d,h,k,l,e,m,c,f;a.ajax({type:"GET",url:"https://kuler-api.adobe.com/rss/search.cfm?searchQuery=userID:102986&itemsPerPage=50&key=5F8FD294DC6015C63AEF97E329246996",dataType:"xml"}).done(function(b){if(!b.error)return b=a(b).find("item"),a.each(b,function(b,n){g=a(this);d=g[0];h=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","themeID")[0].valueOf().innerHTML.toString();k=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","themeTitle")[0].valueOf().innerHTML.toString();
+m="//color.adobe.com/themeID/"+h;e=a('<div id="quartz'+b+'"></div>').addClass("tinge");c=a('<div id="title'+b+'"></div>').addClass("tetra");f=a("<a>").attr("href",m).addClass("tange");f.append(a("<span>").text(k).addClass("titre"));c.append(f);l=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","swatch");a.each(l,function(b,d){var c=a(this)[0].getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","swatchHexColor")[0].valueOf().innerHTML.toString();e.append(a("<div>").css("background-color",
+"#"+c).addClass("scalar"));tumultu.push(c)});c.append(e);a("div#kulerfeed").append(c)}),tumultu})})})(jQuery);
 
 // Processing
 function preload() {
@@ -61,11 +61,12 @@ function windowResized() {
   cntnrSize();
 }
 function utistor() {
+  var tinge = tumultu[floor(random(tumultu.length))];
   var r, g, b;
-  r = random(25,255);
-  g = random(25,255);
-  b = random(25,255);
-  return color(r, g, b);
+  r = red(tinge);
+  g = green(tinge);
+  b = random(tinge);
+  return color(r,g,b);
 }
 function cntnrSize() {
   cH = cntnr.height;
