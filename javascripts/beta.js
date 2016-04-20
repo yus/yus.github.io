c01e025fcc2cae9e685234edcc0bc7d2c18b5ec2
yus <swiftcase@gmail.com>
2016-04-20 22:32:01 +0800

    beta29

--- javascripts/beta.js
+++ javascripts/beta.js
@@ -2,12 +2,55 @@
  *  Name: Yusdesign Kuler Feed Javascript Processing
  *  License: CC-NC-ND 3.0 Unported
  */
-var utistor, cnv, img, cntnr, gesso;
-var cW, cH, rc, bg, d, sclr, tinge, tincture;
-var tumultu = [], distances = [], maxDistance, spacer;
+
 // Yusdesign jQuery Kuler Feed
+var ut = [];
 jQuery.noConflict();
-(function(a){a(function(){a("body").addClass("yusdesign");var g,d,h,k,l,e,m,c,f;a.ajax({type:"GET",url:"https://kuler-api.adobe.com/rss/search.cfm?searchQuery=userID:102986&itemsPerPage=50&key=5F8FD294DC6015C63AEF97E329246996",dataType:"xml"}).done(function(b){if(!b.error)return b=a(b).find("item"),a.each(b,function(b,n){g=a(this);d=g[0];h=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","themeID")[0].valueOf().innerHTML.toString();k=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","themeTitle")[0].valueOf().innerHTML.toString();
-m="//color.adobe.com/themeID/"+h;e=a('<div id="quartz'+b+'"></div>').addClass("tinge");c=a('<div id="title'+b+'"></div>').addClass("tetra");f=a("<a>").attr("href",m).addClass("tange");f.append(a("<span>").text(k).addClass("titre"));c.append(f);l=d.getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","swatch");a.each(l,function(b,d){var c=a(this)[0].getElementsByTagNameNS("http://kuler.adobe.com/kuler/API/rss/","swatchHexColor")[0].valueOf().innerHTML.toString();e.append(a("<div>").css("background-color",
-"#"+c).addClass("scalar"));tumultu.push(c)});c.append(e);a("div#kulerfeed").append(c)}),tumultu})})})(jQuery);
-console.log(tumultu);
+(function ($) {
+  $(function () {
+    $('body').addClass('yusdesign');
+    var ns = 'http://kuler.adobe.com/kuler/API/rss/',
+    qc = '?searchQuery=userID:102986',
+    qn = '&itemsPerPage=50',
+    qk = '&key=5F8FD294DC6015C63AEF97E329246996';
+    var qu = 'https://kuler-api.adobe.com/rss/search.cfm' + qc + qn + qk,
+    $book, book, $ns_themeID, $ns_themeTitle, $ns_swatches, quler, themeLink, qlrtitle, qlr;
+    $.ajax({
+      type: 'GET',
+      url: qu,
+      dataType: 'xml'
+    }).done(function (result) {
+      if (!result.error) {
+        var $books = $(result).find('item');
+        $.each($books, function (i, j) {
+          $book = $(this);
+          book = $book[0];
+          $ns_themeID = book.getElementsByTagNameNS(ns, 'themeID')[0].valueOf().innerHTML.toString();
+          $ns_themeTitle = book.getElementsByTagNameNS(ns, 'themeTitle')[0].valueOf().innerHTML.toString();
+          themeLink = '//color.adobe.com/themeID/' + $ns_themeID;
+          quler = $('<div id="quartz' + i + '"></div>').addClass('tinge');
+          qlrtitle = $('<div id="title' + i + '"></div>').addClass('tetra');
+          qlr = $('<a>').attr( 'href', themeLink ).addClass('tange');
+          qlr.append( $('<span>').text( $ns_themeTitle ).addClass('titre') );
+          qlrtitle.append(qlr);
+          $ns_swatches = book.getElementsByTagNameNS(ns, 'swatch');
+          $.each($ns_swatches, function (q, r) {
+            var $swatch = $(this);
+            var swatch = $swatch[0];
+            var $swtch = swatch.getElementsByTagNameNS(ns, 'swatchHexColor')[0].valueOf().innerHTML.toString();
+            quler.append($('<div>').css('background-color', '#' + $swtch).addClass('scalar'));
+            $(ut).append($swtch);
+            console.log($(ut)[q]);
+          });
+          qlrtitle.append(quler);
+          $('div#kulerfeed').append( qlrtitle );
+        });
+        
+        $.each( $(ut), function( index, value ) {
+          console.log( "index", index, "value", value );
+        });
+      }
+    });
+    
+  });
+})(jQuery);
