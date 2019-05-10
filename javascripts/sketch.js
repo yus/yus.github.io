eba0647780205668ae9a1b6c4ef113ac11ebb45e
yus <swiftcase@gmail.com>
2019-05-10 10:11:16 +0800

    banner added
    BitMiner

--- javascripts/sketch.js
+++ javascripts/sketch.js
@@ -24,10 +24,15 @@ function setup() {
   cnvs.parent(cnt).position(0,120).background(52);
   var hdr = createDiv('').id('header').parent(cnt);
   select('#header').size(windowWidth,120).position(0,0);
+  
   var ftr = createDiv('').id('footer').parent(cnt);
   select('#footer').size(windowWidth,100).position(0,windowHeight-100);
   var logo = createImg('images/yus143.png').parent('#header').position(72,29);
-
+  var btmnr = createDiv( createA('https://bitminer.io/3636945',
+                                 createImg('https://bitminer.io/s/bitminer_4.gif',
+                                           'BitMiner - free and simple next generation Bitcoin mining software'),
+                                 '_blank') ).parent('#header').position(windowWidth-498,30);
+  
   var rlgh = createA('https://github.com/',
                       '<img src="images/ghmarkw.png" alt="github" height="29">')
     .parent('#footer').position(72,29);
