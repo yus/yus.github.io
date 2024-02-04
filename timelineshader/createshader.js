bfe6f9495a9f1ae3b3f387c71b62335229b4e494
yus <swiftcase@gmail.com>
2024-02-04 10:37:21 +0800

    Update createshader.js
    Signed-off-by: yus <swiftcase@gmail.com>

--- timelineshader/createshader.js
+++ timelineshader/createshader.js
@@ -1,4 +1,3 @@
-
 // the vertex shader is called for each vertex
 let vs = `precision highp float;
 uniform mat4 uModelViewMatrix;
@@ -13,11 +12,31 @@ void main() {
   vec4 positionVec4 = vec4(aPosition, 1.0);
   gl_Position = uProjectionMatrix * uModelViewMatrix * positionVec4;
  }`;
+
 // the fragment shader is called for each pixel
-let fs = ``;
+let fs = `precision highp float;
+uniform vec2 p;
+uniform float r;
+const int I = 500;
+varying vec2 vTexCoord;
+   
+void main() {
+  vec2 c = p + gl_FragCoord.xy * r, z = c;
+  float n = 0.0;
+  for (int i = I; i > 0; i --) {
+    if(z.x*z.x+z.y*z.y > 4.0) {
+      n = float(i)/float(I);
+      break;
+    }
+    z = vec2(z.x*z.x-z.y*z.y, 2.0*z.x*z.y) + c;
+  }
+  gl_FragColor = vec4(0.5-cos(n*17.0)/2.0,0.5-cos(n*13.0)/2.0,0.5-cos(n*23.0)/2.0,1.0);
+}`;
+
 let mandel;
+
 function setup() {
-  createCanvas(100, 100, WEBGL);
+  createCanvas(300, 300, WEBGL);
 
   // create and initialize the shader
   mandel = createShader(vs, fs);
