93e2303022e5f0c31be370598e0f258169c7c22f
yus <swiftcase@gmail.com>
2024-02-06 03:37:34 +0800

    Update colors.js
    Signed-off-by: yus <swiftcase@gmail.com>

--- javascripts/colors.js
+++ javascripts/colors.js
@@ -1,3 +1,15 @@
 // yusdesign colors, p5*js
 
 let colors;
+
+function setup() {
+  let gta = [];
+  gta.push(select('.group-title'));
+  let gtdivs = selectAll('.group-title'); 
+  for (let i = 0; i < gta.length; i += 1) {
+    let a = select(gtdivs[i].html());
+    gtdivs[i].html('<div class = "lbl"></div>' + a);
+  } 
+  
+  select('.lbl').style('background-image', './images/label_straight_left.svg');
+} 
