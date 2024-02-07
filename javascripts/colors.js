4b2957543a744b21e5fdec8f10f8d34690aeea11
yus <swiftcase@gmail.com>
2024-02-07 14:39:42 +0800

    Update colors.js
    Signed-off-by: yus <swiftcase@gmail.com>

--- javascripts/colors.js
+++ javascripts/colors.js
@@ -1,15 +1,18 @@
 // yusdesign colors, p5*js
 
-let colors;
+let colors = [];
+let gta = [];
 
 function setup() {
-  let gta = [];
-  gta.push(select('.group-title'));
-  let gtdivs = selectAll('.group-title'); 
+  if(elt.hasClass('group_title'))
+    colors.push(select(this.html());
+    console.log(colors);
+    gta.push(select(this));
+  //let gtdivs = selectAll('.group-title'); 
   for (let i = 0; i < gta.length; i += 1) {
-    let a = select(gtdivs[i].html());
-    gtdivs[i].html('<div class = "lbl"></div>' + a);
+    let a = select(gta[i]);
+    a.html('<div class = "lbl"></div>', true);
   } 
-  
-  select('.lbl').style('background-image', './images/label_straight_left.svg');
+  let labelbg = './images/label_straight_left.svg';
+  select('lbl').style('background-image', labelbg);
 } 
