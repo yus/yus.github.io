cd942b6e4c23b4414ccbd7b3398ef73ee4bef75d
yus <1069227+yus@users.noreply.github.com>
2025-08-22 23:34:53 +0800

    some updates

--- javascripts/colors.js
+++ javascripts/colors.js
@@ -5,14 +5,14 @@ let gta = [];
 
 function setup() {
   if(elt.hasClass('group_title'))
-    colors.push(select(this.html());
+    colors.push(select(this.html()));
     console.log(colors);
     gta.push(select(this));
-  //let gtdivs = selectAll('.group-title'); 
+  //let gtdivs = selectAll('.group-title');
   for (let i = 0; i < gta.length; i += 1) {
     let a = select(gta[i]);
     a.html('<div class = "lbl"></div>', true);
-  } 
+  }
   let labelbg = './images/label_straight_left.svg';
   select('lbl').style('background-image', labelbg);
-} 
+}
